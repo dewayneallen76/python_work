26c8f9799fb453c147083b7b0cbfcbdc827092d8
Dewayne Allen <dewayneallen76@gmail.com>
2025-06-06 14:10:30 -0400

    remove prefix exercise

---
+++ remove_prefix.py
@@ -0,0 +1,6 @@
+# When working with strings, another common task is to remove a prefix. 
+# This can be done with the removeprefix() function.
+nostarch_url = 'https://nostarch.com'
+print(nostarch_url.removeprefix('https://'))
+
+# In this example we are removing https://
