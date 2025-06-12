767a067f80d410038022ba938e2915751b417bce
Dewayne Allen <dewayneallen76@gmail.com>
2025-06-12 15:59:27 -0400

    Ch 4 exercises, pizzas for loop

---
+++ Ch_4_Working_with_Lists/pizzas.py
@@ -0,0 +1,12 @@
+# Chapter 4 exercises
+# TODO This of at least three kinds of your favorite pizza
+# TODO Store these pizza names in a list
+# TODO Use a for loop to print the name of each pizza
+
+pizzas = ["pepporoni", "house special", "cheese"]
+for pizza in pizzas:
+    print(pizza.title())
+    # TODO modify your for loop to print a sentence use the names of the pizzas
+    print(f"I like {pizza.title()}.\n")
+# TODO add a line at the end of your program that states how much you like pizza
+print("I really love pizza!")
