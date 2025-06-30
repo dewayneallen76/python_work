be781af45240469e6ee9afb6659850530a7a409e
Dewayne Allen <dewayneallen76@gmail.com>
2025-06-30 16:05:43 -0400

    ch 5. if and if else statements

---
+++ Ch_5_If_statements/voting.py
@@ -0,0 +1,10 @@
+age = 17
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote?")
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please register to voe as soon as you turn 18!")
+
+# An if-else block is similar to a simple if statement, but the else statement allows you to define
+# an action or set of actions that are executed when the conditional test fails. 
