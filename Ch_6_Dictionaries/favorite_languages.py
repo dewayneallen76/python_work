34c9dbc04331cc1819a78a9769f5da7772ac048f
Dewayne Allen <dewayneallen76@gmail.com>
2025-07-08 17:26:34 -0400

    Ch 6. Looping through all the keys in a dictionary using keys()

--- Ch_6_Dictionaries/favorite_languages.py
+++ Ch_6_Dictionaries/favorite_languages.py
@@ -13,3 +13,24 @@ print(f"Sarah's favorite programming language is {language}")
 # Looping through a dictionary using items() method:
 for name, language in favorite_language.items():
     print(f"{name.title()}'s favorite programming language is {language.title()}")
+
+# Looping through all the keys in a dictionary
+# The keys() method is useful when you don't need to work with all of the values in a dictionary.
+friends = ["phil", "sarah"]
+for name in favorite_language.keys():
+    print(f"Hi {name.title()}.")
+# You can access the value associated with any key you care about inside the loop, by using the 
+# current key. 
+    if name in friends:
+        language = favorite_language[name].title()
+        print(f"\t{name.title()}, I see you love {language}")
+
+if "erin" not in favorite_language.keys():
+    print("Erin, please take our poll!")
+
+# Looping through keys is actually the default behavior when looping through a dictionary, so this
+# code would have exactly the same output as above: 
+for name in favorite_language:
+    print(name.title())
+
+
