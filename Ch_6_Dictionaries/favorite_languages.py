6f4ea08206906a3efccbb6c5acf1cff9d41e4019
Dewayne Allen <dewayneallen76@gmail.com>
2025-07-08 16:30:17 -0400

    Ch 6 using a dictionary for similar objects

---
+++ Ch_6_Dictionaries/favorite_languages.py
@@ -0,0 +1,12 @@
+# You can also use a dictionary to store one kind of information about many objects. For example,
+# if you wanted to poll a group of people and ask them what their favorite programming language is.
+# A dictionary is useful for storing the results of a simple poll, like this: 
+favorite_language = {
+    "jen": "python", 
+    "sarah": "c", 
+    "edward": "rust", 
+    "phil": "python",
+}
+
+language = favorite_language["sarah"].title()
+print(f"Sarah's favorite programming language is {language}")
