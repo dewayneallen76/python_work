dd8d86a1a393b0d60133923ca2351b964ba9eace
Dewayne Allen <dewayneallen76@gmail.com>
2025-07-09 16:52:58 -0400

    Ch 6 Nesting dictionaries inside dictionaries

---
+++ Ch_6_Dictionaries/many_users.py
@@ -0,0 +1,27 @@
+# You can nest a dictionary inside another dictionary, but your code can get complicated quickly
+# when you do. 
+users = {
+    'aeinstein': {
+        'first': 'albert',
+        'last': 'einstein',
+        'location': 'princeton',
+        },
+
+    'mcurie': {
+        'first': 'marie',
+        'last': 'curie',
+        'location': 'paris',
+        },
+
+    }
+
+for username, user_info in users.items():
+    print(f"\nUsername: {username}")
+    full_name = f"{user_info['first']} {user_info['last']}"
+    location = user_info['location']
+
+    print(f"\tFull name: {full_name.title()}")
+    print(f"\tLocation: {location.title()}")
+
+# Notice that the structure of each user's dictionary is identical. Although not required by Python
+# this structure makes nested dictionaries easier to work with. 
