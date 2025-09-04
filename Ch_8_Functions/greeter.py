062d62887e7408018cede7ab893ff9ebf2a12695
Dewayne Allen <dewayneallen76@gmail.com>
2025-09-04 17:59:38 -0400

    Ch 8 moving files to correct folder

--- Ch_8_Functions/greeter.py
+++ Ch_8_Functions/greeter.py
@@ -1,44 +1,24 @@
-# Functions are named blocks of code designed to do one specific job. When you want to perform
-# a particular tas that you've defined in a function, you call the function responsible for it. 
-# If you need to perform that task multiple times throughout your program, you don't need to type
-# all the code for the same task again and again. 
+# Using a Function with a while Loop
+#
+# You can use functions with all the Python structures you've learned about so far. For example, 
+# using the get_formatted_name() function from previous lesson formatted_name.py we can add a while
+# loop to greet users more formally 
+from Ch_8_Functions.formatted_name import get_formatted_name
 
-# Defining a function
-def greet_user():
-    # docstring below describes what the funtion does 
-    """Display a simple greeting"""
-    print("Hello!")
+while True:
+    print("\nPlease tell me your name:")
+    print("(enter 'q' at any time to quit)")
+    
+    f_name = input("First Name: ")
+    if f_name == 'q':
+        break
+    
+    l_name = input("Last Name: ")
+    if l_name == 'q':
+        break
 
-# Passing information to a function
-# By modifying the above function slightly it can greet the user by name. For the function to do 
-# this, you enter username in the parentheses of the function's definition. By adding username, you
-# allow the function to accept any value of username you specify. The function now expects you to
-# provide a value for username each time the function is called. 
-# Rewriting the above function
-def greet_user_2(username):
-    """Display a simple greeting passing value"""
-    print(f"Hello, {username.title()}!")
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name}!")
 
-greet_user()
-greet_user_2("dewayne")
 
-# 8-1 pg. 131
-# Write a function called display_message() that prints one sentence telling everyone what you are
-# learning about in this chapter. 
 
-def display_message():
-    """Tell everyone what is in this chapter"""
-    print("This chapter is about Python functions.")
-
-display_message()
-
-#8-2 pg. 131
-# Write a function called favorite_book() that accepts one parameter, title. The function should
-# print a message, such as "One of my favorite books is Dungeon Crawler Carl". Call the function,
-# making sure to include a book title as an argument in the function call. 
-
-def favorite_book(title):
-    """Accepts value title to display message of favorite book"""
-    print(f"One of my favorite books is {title.title()}.")
-
-favorite_book("salems lot")
