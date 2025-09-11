a33ea41a621a9d084febd1b2eb86f738d7f2dcd0
Dewayne Allen <dewayneallen76@gmail.com>
2025-09-11 12:48:58 -0400

    Ch 8 Try it yourself exercises tasks write up

---
+++ Ch_8_Functions/try_it_yourself_messages.py
@@ -0,0 +1,18 @@
+# Try it yourself (Lists with functions)
+
+# 8-9. Messages
+# Make a list containing a series of short text messages. Pass the list to a function called 
+# show_messages(), which prints each text message. 
+
+
+# 8-10. Sending Messages
+# Start with a copy of your program from Exercise 8-9. Write a function called send_messages() that
+# prints each text message and moves each message to a new list called sent_messages() as it's 
+# printed. After calling the function, print both of your lists to make sure that the messages
+# were moved correctly. 
+
+
+# 8-11. Archived Messages
+# Start with your work from Excercise 8-10. Call the function send_messages() with a copy of the
+# list of messages. After calling the function, print both of your lists to show that the original
+# list has retained its messages. 
