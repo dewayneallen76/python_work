88e03345c9c41db3e844c1f48e749ab54a111e50
Dewayne Allen <dewayneallen76@gmail.com>
2025-09-22 17:24:03 -0400

    Ch 8 arbitrary number of arguments and mixing positinal and arbitrary arguments

--- Ch_8_Functions/pizza.py
+++ Ch_8_Functions/pizza.py
@@ -15,3 +15,21 @@ make_pizza('mushrooms', 'green peppers', 'extra cheese', 'banana peppers')
 
 # The asterisk in the parameter name *toppings tells Python to make a tuple called toppings, 
 # containing all the values this function receives. 
+
+# Mixing Positional and Arbitrary Arguments 
+# 
+# If you want a function to accept several different kinds of arguments, the parameter that accepts
+# an arbitray number of arguments must be placed last in the function definition. Python matches
+# positional and keyword arguments first and then collects any remaining arguments in the final 
+# parameter. 
+# For example, if the function needs to take a size for the pizza, that parameter must come before
+# the parameter *toppings
+
+def size_pizza(size, *toppings):
+    """Summarize the size and toppings for a pizza"""
+    print(f"\nMaking a {size}-inch pizza with the following toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
+
+size_pizza(12, 'pepperoni')
+size_pizza(16, 'pepperoni', 'ham', 'bacon', 'banana peppers')
