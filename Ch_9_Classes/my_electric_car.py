a116c0f20bd7bf5182c2c8e72f0ac4990381d926
Dewayne Allen <dewayneallen76@gmail.com>
2026-01-07 15:58:13 -0500

    Added notes for using aliases for modules.

--- Ch_9_Classes/my_electric_car.py
+++ Ch_9_Classes/my_electric_car.py
@@ -4,3 +4,11 @@ my_tesla = ElectricCar('tesla', 'model s', 2022)
 print(my_tesla.get_descriptive_name())
 my_tesla.battery.describe_battery()
 my_tesla.battery.get_range()
+
+# Using Aliases for Modules
+# If the module name is long or might conflict with another module name, you can use an alias when importing it.
+# For example, you can import the new_car module with an alias like this:
+# import new_car as nc
+# Then you can create instances of the classes using the alias:
+# my_tesla = nc.ElectricCar('tesla', 'model s', 2022)
+# This approach helps keep your code concise and avoids naming conflicts.
