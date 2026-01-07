4029a800ecb033c31026a8a0ef8592352fdf9de5
Dewayne Allen <dewayneallen76@gmail.com>
2026-01-07 15:48:40 -0500

    Importing all classes in a module with asterisk

--- Ch_9_Classes/my_cars.py
+++ Ch_9_Classes/my_cars.py
@@ -9,3 +9,14 @@ my_gas_car = Car('ford', 'mustang', 2021)
 print(my_gas_car.get_descriptive_name())
 my_electric_car = ElectricCar('tesla', 'model 3', 2022)
 print(my_electric_car.get_descriptive_name())
+
+# Importing All Classes from a Module
+# If you want to import all classes from a module, you can use the asterisk (*) wildcard character.
+from new_car import *
+# This imports all classes defined in the new_car module. Now you can create instances of any class defined in that module.
+another_gas_car = Car('honda', 'civic', 2020)
+print(another_gas_car.get_descriptive_name())
+another_electric_car = ElectricCar('chevrolet', 'bolt', 2021)
+print(another_electric_car.get_descriptive_name())
+# However, using the asterisk (*) to import everything is generally discouraged, as it can lead to confusion and make your code less readable.
+# It's usually better to import only the specific classes you need.
