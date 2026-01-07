7c77214e48de5f514251233bb14d2d5531b027af
Dewayne Allen <dewayneallen76@gmail.com>
2026-01-07 15:33:01 -0500

    Added notes.

--- Ch_9_Classes/my_car.py
+++ Ch_9_Classes/my_car.py
@@ -4,3 +4,6 @@ my_new_car = Car('subaru', 'outback', 2024)
 print(my_new_car.get_descriptive_name())
 my_new_car.update_odometer(500)
 my_new_car.read_odometer()
+
+# Importing classes is an effective way to program. It allows you to keep your code organized and modular,
+# making it easier to maintain and reuse. As your projects grow in complexity, using modules and imports will become increasingly important.
